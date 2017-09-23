bbf366a19375a28d8d74394eb4232e0403d44f99
Sergey Romanenko <sergey.romanenko@gmail.com>
2017-09-23 19:46:08 +0300

    23/09/2017 + HTML + Listener

--- Listener/elementListener.js
+++ Listener/elementListener.js
@@ -1,35 +1,47 @@
-function viewGrid() {
+/*function viewGrid() {
     let newContent="";
+    this.preventDefault();
+    let endString='Привте братва !!!';
+    document.getElementById('content').innerHTML = '<b>Привте братва</b>';
+
+    grid.innerHTML = endString;
 
 }
-function fshowList() {
-let newContent="";
 
-}
 
 function finsertRec() {
     let newContent="";
+    this.preventDefault();
 
 }
 function fdeleteRec() {
     let newContent="";
+    this.preventDefault();
 
 }
 function ffilterRec() {
     let newContent="";
+    this.preventDefault();
+
+}
+*/
+function fshowList() {
 
 }
 
-let elShowList = document.getElementById('showList'); // оторбразить список
-elShowList.addEventListener('click',fshowList, false);
+var elShowList = document.getElementById('showList'); // оторбразить список
+elShowList.addEventListener('click',function(){
+    fshowList();
+    }, false);
 
-let elinsertRec = document.getElementById('insertRec'); // оторбразить список
-elinsertRec.addEventListener('click',finsertRec, false);
+/*let elinsertRec = document.getElementById('insertRec'); // оторбразить список
+elinsertRec.addEventListener('click',finsertRec(), false);
 
 let eldeleteRec = document.getElementById('deleteRec'); // оторбразить список
-eldeleteRec.addEventListener('click',fdeleteRec, false);
+eldeleteRec.addEventListener('click',fdeleteRec(), false);
 
 let elfilterRec = document.getElementById('filterRec'); // оторбразить список
-elfilterRec.addEventListener('click',ffilterRec, false);
+elfilterRec.addEventListener('click',ffilterRec(), false);
 
-window.addEventListener('load', viewGrid, false);
+let grid = document.getElementById('content');
+window.addEventListener('load', viewGrid(), false);*/
