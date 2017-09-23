9e3cfb8f8ac0e997de5e7da03bafeae9e73cbdf0
Sergey Romanenko <sergey.romanenko@gmail.com>
2017-09-23 18:56:09 +0300

    23/09/2017 + HTML + Listener

--- Listener/elementListener.js
+++ Listener/elementListener.js
@@ -5,3 +5,12 @@ var newContent="";
 
 var elShowList = document.getElementById('showList'); // оторбразить список
 elShowList.addEventListener('click',showList, false);
+
+var elinsertRec = document.getElementById('insertRec'); // оторбразить список
+elinsertRec.addEventListener('click',showList, false);
+
+var eldeleteRec = document.getElementById('deleteRec'); // оторбразить список
+eldeleteRec.addEventListener('click',showList, false);
+
+var elfilterRec = document.getElementById('filterRec'); // оторбразить список
+elfilterRec.addEventListener('click',showList, false);
