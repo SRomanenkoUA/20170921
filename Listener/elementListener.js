c043a6b46ff3096b673dc1d44d948dbecd8b1f8f
Sergey Romanenko <sergey.romanenko@gmail.com>
2017-09-23 18:58:21 +0300

    23/09/2017 + HTML + Listener

--- Listener/elementListener.js
+++ Listener/elementListener.js
@@ -1,16 +1,30 @@
-function showList() {
+function fshowList() {
 let newContent="";
 
 }
 
+function finsertRec() {
+    let newContent="";
+
+}
+function fdeleteRec() {
+    let newContent="";
+
+}
+function ffilterRec() {
+    let newContent="";
+
+}
+
+
 let elShowList = document.getElementById('showList'); // оторбразить список
-elShowList.addEventListener('click',showList, false);
+elShowList.addEventListener('click',fshowList, false);
 
 let elinsertRec = document.getElementById('insertRec'); // оторбразить список
-elinsertRec.addEventListener('click',showList, false);
+elinsertRec.addEventListener('click',finsertRec, false);
 
 let eldeleteRec = document.getElementById('deleteRec'); // оторбразить список
-eldeleteRec.addEventListener('click',showList, false);
+eldeleteRec.addEventListener('click',fdeleteRec, false);
 
 let elfilterRec = document.getElementById('filterRec'); // оторбразить список
-elfilterRec.addEventListener('click',showList, false);
+elfilterRec.addEventListener('click',ffilterRec, false);
